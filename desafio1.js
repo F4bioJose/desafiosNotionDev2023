d62fc7a108b17fc3b739da9b2fc3a288eb170221
Fábio José <fabio02d2@gmail.com>
2023-07-26 23:47:44 -0300

    Challenge 1 is done

--- desafio1.js
+++ desafio1.js
@@ -1,3 +1,34 @@
-function calculetSalary() {
+
+function calculetSalary(hourValue,hoursWorked) {
+
+    let grossSalary = hourValue * hoursWorked;
+    let rate = 0;
     
+    if (grossSalary <= 900) {
+        rate = 0;
+    } else if (grossSalary <= 1500) {
+        rate = 5/100
+    } else if (grossSalary <= 2500) {
+        rate = 1/10
+    } else if (grossSalary > 2500) {
+        rate = 2/10
+    } 
+
+    const incomeTax = grossSalary*(rate);
+    const syndicate = grossSalary*(3/100);
+    const totalDiscount = incomeTax + syndicate;
+    const netSalary = grossSalary - totalDiscount;
+    const fgts = grossSalary*(11/100);
+
+    console.log(`Gross Salary (${hourValue}*${hoursWorked}):  R$${grossSalary}`)
+    console.log(`IR :  R$${incomeTax}`)
+    console.log(`( - ) Syndicate :  3%`)
+    console.log(`FGTS : 11%`)
+    console.log(`Total Discount :  ${totalDiscount}`)
+    console.log(`Net Salary : ${netSalary}`)
+
+    return
 }
+    
+calculetSalary(6.55,168)
+
