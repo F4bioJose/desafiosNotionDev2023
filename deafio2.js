4b6a8e0989ab47daec3a2449548812de019f8fe9
Fabio.Jose <fabiojas@ufba.br>
2023-07-29 20:00:26 -0300

    inital commit to desafio2

---
+++ deafio2.js
@@ -0,0 +1,14 @@
+// calc1 = new Calculadora([7, 8, 10, 24, 21, 38, 157, 3, 16]);
+// calc1.printAll();
+
+// Fatorial do primeiro elemento: 5040
+// Média aritimética: 31.555555555555557
+// Variância 2069.58024691358
+// Desvio padrão 45.49263948061906
+// Números ímpares: 7,21,157,3
+
+class Calculadora {
+
+}
+
+console.log(typeof Calculadora)
