a62ed0d732b1ef4bd795901970bfa692d126af66
Fabio.Jose <fabiojas@ufba.br>
2023-08-01 22:57:10 -0300

    error corrected in fatorial method

--- deafio2.js
+++ deafio2.js
@@ -1,13 +1,90 @@
-// calc1 = new Calculadora([7, 8, 10, 24, 21, 38, 157, 3, 16]);
-// calc1.printAll();
+class Calculadora {
+    constructor(arrayNumerica) {
+        this.arrayNumerica = arrayNumerica;
+    }
 
-// Fatorial do primeiro elemento: 5040
-// Média aritimética: 31.555555555555557
-// Variância 2069.58024691358
-// Desvio padrão 45.49263948061906
-// Números ímpares: 7,21,157,3
+    fatorial() {
+    // Fatorial 
+    let primeiroNumero = this.arrayNumerica[0]
 
-function fatorial(numero) {
+    if(primeiroNumero === 0 || primeiroNumero === 1) {
+        return 1
+
+    }else {
+        let fatorial = 1
+        for(let i = 1; i <= primeiroNumero; ++i) {
+        fatorial *= i;
+        }
+
+        return fatorial
+     }
+    } 
+
+    mediaAritmedica() {
+    // Média Aritmédica 
+        let soma = 0
+        for(let i = 0; i < this.arrayNumerica.length; i++) {
+            soma += this.arrayNumerica[i]
+        }
+        let media = soma/this.arrayNumerica.length
+        return media
+    } 
+   
+    variancia() {
+        let media = this.mediaAritmedica();
+        let desvioArray = [];
+
+        for (let i = 0; i < this.arrayNumerica.length; i++) {
+           let desvio = media - this.arrayNumerica[i]
+           desvioArray.push(desvio ** 2)
+        }
+          
+        let variancia = 0;
+        for (let i = 0; i < desvioArray.length; i++) {
+            variancia += desvioArray[i];
+        }   
+        variancia /= this.arrayNumerica.length
+        return variancia
+    }
+
+    desvioPadrao () {
+        const variancia = this.variancia();
+        let desvioPadrao = variancia**(1/2)
+        
+        return desvioPadrao
+    }
+
+    obterNumerosImpares() {
+        
+        let numerosImpares = []
+        for(let i = 0; i < this.arrayNumerica.length; i++) {
+            if(this.arrayNumerica[i]%2 !== 0 ) {
+                numerosImpares.push(this.arrayNumerica[i])
+            }
+        }
     
-}
+        return numerosImpares
+    }
+    
 
+    printAll() {
+
+       const fatorial = this.fatorial();
+       console.log(`Fatorial do primeiro elemento: ${fatorial}`)
+
+       const media = this.mediaAritmedica();
+       console.log(`A Média aritmédica é ${media}`)
+       
+       const variancia = this.variancia();
+       console.log(`Variância: ${variancia}`)
+
+       const desvioPadrao = this.desvioPadrao();
+       console.log(`Desvio Padrão: ${desvioPadrao}`)
+
+       const numerosImpares = this.obterNumerosImpares();
+       console.log(`Números impares: ${numerosImpares}`)
+    }
+
+}
+    calc1 = new Calculadora([2,3,1,9,6,5,4])
+    calc1.printAll();
